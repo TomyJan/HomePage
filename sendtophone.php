87eca7ce414bf7f8362774522f3b26804ed7168d
TomyJan <TomyJan6@gmail.com>
2024-11-16 10:25:14 +0800

    refactor: move codes

--- sendtophone.php
+++ sendtophone.php
@@ -1,6 +1,6 @@
 <?php
 $msg = $_GET['msg'];
-// TODO
+// TODO, 老地址保留, 需要设置在 cdn 跳转到 .json, 并触发警告
 header("Content-Type:text/json;charset=UTF-8");
-echo {"code":0,"content":{"result":["{\"counts\":1,\"logs\":[],\"success\":\"ok\"}"]}};
+echo {"code":-999,"content":{"result":["{\"counts\":1,\"logs\":[\"invalid api\"],\"success\":\"no\"}"]}};
 ?>
