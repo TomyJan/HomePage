e2df8364d759b687f80dc72f7f267e2adf6810fa
TomyJan <TomyJan6@gmail.com>
2024-11-21 19:17:50 +0800

    chore: rm useless files

--- sendtophone.php
+++
@@ -1,6 +0,0 @@
-<?php
-$msg = $_GET['msg'];
-// TODO, 老地址保留, 需要设置在 cdn 跳转到 .json, 并触发警告
-header("Content-Type:text/json;charset=UTF-8");
-echo {"code":-999,"content":{"result":["{\"counts\":1,\"logs\":[\"invalid api\"],\"success\":\"no\"}"]}};
-?>
